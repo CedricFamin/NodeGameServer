40ef96a620e42ad1971223ee92c7bf9695b01b35
Cedric Famin <cedric.famin@ubisoft.com>
2016-08-22 15:11:03 +0200

    [Monitor] Set up monitor on daemon

--- Servers/Daemon/server.js
+++ Servers/Daemon/server.js
@@ -23,28 +23,4 @@ srv.init();
 Handlers.set(srv);
 srv.launch();
 
-// Low memory warning monitor
-require('monitor').start();
-var Monitor = require('monitor');
-var LOW_MEMORY_THRESHOLD = 4800000000;
-
-// Set the probe to push changes every 10 seconds
-var options = {
-  hostName: 'localhost',
-  probeClass: 'Process',
-  initParams: {
-    pollInterval: 10
-  }
-}
-var processMonitor = new Monitor(options);
-
-var freemem = processMonitor.get('freemem');
-console.log('Low memory warning: ' + freemem);
-
-// Now connect the monitor
-processMonitor.connect(function(error) {
-  if (error) {
-    console.error('Error connecting with the process probe: ', error);
-    process.exit(1);
-  }
-});
+var monitor = require("../../uptime/monitor");
